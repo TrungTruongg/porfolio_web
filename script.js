6fc01093a9bc5d1fa6cf8eb574ed67a55ce7c46a
TRUNG <trungtt089@gmail.com>
2026-06-18 16:56:49 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,112 @@
+const personalProjects = [
+  {
+    title: "Project Management System",
+    description: "Task and project management platform.",
+    image: "./assets/mytask.png",
+    link: "https://project-management-system-orpin.vercel.app/",
+    tech: ["React", "Express", "NodeJS", "MongoDB"],
+  },
+  {
+    title: "Tech Blog",
+    description: "Blog management and publishing system.",
+    image: "./assets/tech_blog.png",
+    link: "https://blog-beginner.pages.dev/",
+    tech: ["React", "NodeJS", "SQL"],
+  },
+  {
+    title: "Shopee Mart",
+    description: "Developed an e-commerce platform supporting product browsing, shopping cart, order placement, and user authentication.",
+    image: "./assets/shopee_mart.png",
+    link: "https://shopelo.vercel.app/",
+    tech: ["React", "Redux", "NodeJS", "MongoDB"],
+  },
+];
+
+const companyProjects = [
+  {
+    title: "Leave Management System",
+    description: "Employee leave request and approval workflow.",
+    image: "./assets/qlns.png",
+    tech: ["React", "NodeJS", "MongoDB"],
+  },
+];
+
+const companyContainer = document.getElementById("companyProjects");
+const personalContainer = document.getElementById("personalProjects");
+
+personalProjects.forEach((project) => {
+  personalContainer.innerHTML += `
+<a href="${project.link}" target="_blank" class="project-link">
+  <div class="project-card hidden">
+
+    <div class="project-image">
+      <img src="${project.image}" alt="${project.title}">
+    </div>
+
+    <div class="project-content">
+      <h3>${project.title}</h3>
+      <p>${project.description}</p>
+
+      <div class="tech">
+        ${project.tech.map((t) => `<span>${t}</span>`).join("")}
+      </div>
+    </div>
+
+  </div>
+</a>
+`;
+});
+
+companyProjects.forEach((project) => {
+  companyContainer.innerHTML += `
+<div class="project-card hidden">
+
+    <div class="project-image">
+        <img src="${project.image}" alt="${project.title}">
+    </div>
+
+     <div class="project-content">
+    <h3>${project.title}</h3>
+    <p>${project.description}</p>
+    <div class="tech">
+        ${project.tech.map((t) => `<span>${t}</span>`).join("")}
+    </div>
+    </div>
+
+</div>
+`;
+});
+
+/* ACTIVE NAV */
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav-links a");
+
+window.addEventListener("scroll", () => {
+  let current = "";
+
+  sections.forEach((section) => {
+    const top = section.offsetTop - 150;
+
+    if (scrollY >= top) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach((link) => {
+    link.classList.remove("active");
+
+    if (link.getAttribute("href") === "#" + current) {
+      link.classList.add("active");
+    }
+  });
+});
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    }
+  });
+});
+
+document.querySelectorAll(".hidden").forEach((el) => observer.observe(el));
